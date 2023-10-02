56634dc3d64bdf5ac6ace84b663522a8744f1d9f
Nodirbek <122228871+Nodirbek-Hakimjonov@users.noreply.github.com>
2023-10-02 16:59:12 +0500

    'celery'

--- goodreads/view.py
+++ goodreads/view.py
@@ -6,7 +6,18 @@ from books.models import BookReview
 
 
 def landing_page(request):
-    return render(request, template_name='landing.html')
+    book_reviews = BookReview.objects.all().order_by("-created_at")
+    page_size = request.GET.get('page_size', 10)
+    paginator = Paginator(book_reviews, page_size)
+
+    page_num = request.GET.get('page', 1)
+    page_object = paginator.get_page(page_num)
+    context = {
+        "reviews": book_reviews,
+        "page_obj": page_object,
+    }
+
+    return render(request, "home.html", context)
 def home_page(request):
     book_reviews=BookReview.objects.all().order_by('-created_at')
     page_size=request.GET.get('page_size', 10)
