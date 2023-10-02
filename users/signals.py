48b66d7d3a238d6fae3c112035e3a627cc34d1b1
Nodirbek <122228871+Nodirbek-Hakimjonov@users.noreply.github.com>
2023-10-02 11:28:19 +0500

    'celery'

--- users/signals.py
+++ users/signals.py
@@ -3,13 +3,22 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from users.models import CustomUser
-from users.tasks import send_email
+# from users.tasks import send_email
 
-@receiver(post_save,sender=CustomUser)
-def send_welcome_email(sender,instance,created,**kwargs):
+# @receiver(post_save,sender=CustomUser)
+# def send_welcome_email(sender,instance,created,**kwargs):
+#     if created:
+#         send_email.delay(
+#                 "Welcome to GoodReads clone",
+#                 f"hi, {instance.username}. Welcome to GoodReads clone. Enjoy the books and reviews ",
+#                 [instance.email]
+#             )
+@receiver(post_save, sender=CustomUser)
+def send_welcome_email(sender, instance, created, **kwargs):
     if created:
-        send_email.delay(
-                "Welcome to GoodReads clone",
-                f"hi, {instance.username}. Welcome to GoodReads clone. Enjoy the books and reviews ",
-                [instance.email]
-            )
+        send_mail(
+            f"You have been successfuly registered!",
+            f"Salom {instance.username}\n\nGoodReadsga xush kelibsiz! Ro'yxatdan o'tish jarayonini muvoffaqiyatli tugallandi.\n\nAgar sizning boshqa savollar yoki takliflaringiz bo'lsa https://t.me/javohirtwits kanal izohlariga yozishingiz mumkin.\n\nYana bir bor bizni tanlaganingiz uchun rahmat!\n\nHurmat bilan,\n\n{instance.first_name}\n\nGoodReads Jamoasi\n\n",
+            "coderjek@gmail.com",
+            [instance.email],
+        )
--- users/tasks.py
+++ users/tasks.py
@@ -1,13 +1,13 @@
-from django.core.mail import send_mail
-
-from goodreads.celery1 import app
-
-
-@app.task()
-def send_email(subject,message,recipient_list):
-    send_mail(
-        subject,
-        message,
-        'nodirbekhakimjonovsvm@gmail.com',
-        recipient_list
-    )
+# from django.core.mail import send_mail
+#
+# from goodreads.celery1 import app
+#
+#
+# @app.task()
+# def send_email(subject,message,recipient_list):
+#     send_mail(
+#         subject,
+#         message,
+#         'nodirbekhakimjonovsvm@gmail.com',
+#         recipient_list
+#     )
